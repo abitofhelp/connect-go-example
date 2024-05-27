25ac6ffb9d755c00e69a2675e427ca531217b53c
Sri Krishna <skrishna@buf.build>
2024-05-28 01:35:16 +0530

    Patch

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	greetv1 "github.com/abitofhelp/connect-go-example/bazel-bin/proto/greet/v1/greetv1_go_proto_/connect-go-example/gen/greet/v1"
+
+	greetv1 "github.com/abitofhelp/connect-go-example/gen/greet/v1"
+	greetv1connect "github.com/abitofhelp/connect-go-example/gen/greet/v1/greetv1connect"
 )
 
 func main() {
-
 	r := greetv1.GreetRequest{
 		Name: "Mike",
 	}
-	fmt.Printf("Request: %+v", r.String())
+	fmt.Printf("Request: %+v, %s", r.String(), greetv1connect.GreetServiceName)
 }
